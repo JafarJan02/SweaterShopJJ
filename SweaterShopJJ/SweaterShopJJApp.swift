2a924bf59ebd63cd10423d0cc333457370a600a1
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-27 01:26:14 +0300

    Initial Commit

---
+++ SweaterShopJJ/SweaterShopJJApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweaterShopJJApp.swift
+//  SweaterShopJJ
+//
+//  Created by Jafar on 27.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SweaterShopJJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
