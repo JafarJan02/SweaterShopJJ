19108ee7c187c4c98032d3024e25df99b67e5d75
Jafar Janshanlo <jafar.janshanlo20@gmail.com>
2024-03-27 10:07:22 +0300

    Add products data

--- SweaterShopJJ/models/Product.swift
+++ SweaterShopJJ/models/Product.swift
@@ -13,3 +13,12 @@ struct Product: Identifiable {
     var image: String
     var price: Int
 }
+
+var productList = [Product(name: "Light blue", image: "Sweater1", price: 54),
+                   Product(name: "Sand sweater", image: "Sweater2", price: 89),
+                   Product(name: "Sand sweater", image: "Sweater3", price: 79),
+                   Product(name: "Cream sweater", image: "Sweater4", price: 94),
+                   Product(name: "Brown sweater", image: "Sweater5", price: 99),
+                   Product(name: "Blood sweater", image: "Sweater6", price: 65),
+                   Product(name: "Black sweater", image: "Sweater7", price: 54),
+                   Product(name: "brown sweater", image: "Sweater8", price: 83)]
